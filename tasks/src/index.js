502c2a6409c0d27cd3e31bfcc1daa96ad32d9de4
jaikrishnaverma <jaikrishnaverma>
2022-08-18 18:43:54 +0530

    prime numbers from 1 to 100

--- tasks/src/index.js
+++ tasks/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 // Q1. In index.js create an arrow function for calculating a factorial,
 //  then pass an integer and display its factorial as root element inside heading tag.
@@ -10,9 +9,35 @@ const fact=(num)=>{
 if(num<=1){return 1}
 return (num*fact(num-1))
 }
+// Q2. In index.js make an arrow function Prime() that prints all prime nos. from 1 to 100.
+const Prime=()=>{
+  let primeNumbers="0,1";
+  for (let i = 2; i <= 100; i++)         
+       { 		  	  
+          let counter=0; 	  
+          for(let num =i; num>=1; num--)
+	  {
+             if(i%num===0)
+	     {
+ 		counter = counter + 1;
+	     }
+	  }
+	  if (counter ===2)
+	  {
+	     primeNumbers =primeNumbers+","+i;
+	  }	
+       }	
+  return primeNumbers;
+
+}
+
 
 root.render(
+<div>
 <h1>{fact(12)}</h1>
+
+<h1>Prime No. between 1 to 100: {Prime()}</h1>
+</div>
 );
 
 reportWebVitals();
